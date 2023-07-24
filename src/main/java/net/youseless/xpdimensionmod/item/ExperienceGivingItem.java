d0982957b4a7a27492821fb086d3ca64477b6b05
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2023-07-24 11:00:48 -0400

    Can now consume experience edible without getting the nutrition or saturation. Also can always eat it. You gain experience!!!

--- src/main/java/net/youseless/xpdimensionmod/item/ExperienceGivingItem.java
+++ src/main/java/net/youseless/xpdimensionmod/item/ExperienceGivingItem.java
@@ -4,13 +4,29 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import org.jetbrains.annotations.Nullable;
+
 
 public class ExperienceGivingItem extends Item {
     public ExperienceGivingItem(Properties pProperties) {
-        super(pProperties.food(Items.COOKED_BEEF.getFoodProperties()));
+        super(pProperties.food(new FoodProperties.Builder()
+                .alwaysEat()
+                .nutrition(0)
+                .saturationMod(0)
+                .build()));
     }
+
+    // gives experience when eating
+    @Override
+    public ItemStack finishUsingItem(ItemStack stack, net.minecraft.world.level.Level worldIn, LivingEntity entityLiving) {
+        if (!worldIn.isClientSide && entityLiving instanceof net.minecraft.world.entity.player.Player player) {
+            // Add the desired amount of experience points here
+            int experienceToAdd = 5; // For example, adding 5 experience points
+            player.giveExperiencePoints(experienceToAdd);
+        }
+        return super.finishUsingItem(stack, worldIn, entityLiving);
+    }
+
+
 
 //    @Override
 //    public @Nullable FoodProperties getFoodProperties(ItemStack stack, @Nullable LivingEntity entity) {
