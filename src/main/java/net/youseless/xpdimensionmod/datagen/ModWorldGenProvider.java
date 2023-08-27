bb040967f84addf072e1b2e23d176265c96446ae
ThomasBelk <122413223+ThomasBelk@users.noreply.github.com>
2023-08-27 08:58:06 -0400

    new dimension with a custom biome

--- src/main/java/net/youseless/xpdimensionmod/datagen/ModWorldGenProvider.java
+++ src/main/java/net/youseless/xpdimensionmod/datagen/ModWorldGenProvider.java
@@ -8,6 +8,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.youseless.xpdimensionmod.worldgen.dimension.ModDimensions;
 
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
@@ -16,7 +17,9 @@ public class ModWorldGenProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
 //            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
 //            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
-            .add(Registries.BIOME, ModBiomes::bootstrap);
+            .add(Registries.BIOME, ModBiomes::bootstrap)
+            .add(Registries.LEVEL_STEM, ModDimensions::bootstrapStem)
+            .add(Registries.DIMENSION_TYPE, ModDimensions::bootstrapType);
 //            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
 
     public ModWorldGenProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
